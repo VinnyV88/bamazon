39e443fcbd95c76d87a7b2d9912dcf380814b4f5
vincent_visconti <vinnyv88@gmail.com>
2017-02-17 15:31:33 -0500

    finished part II.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,62 +3,61 @@ var inquirer = require('inquirer');
 require('console.table');
 
 var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'bamazon_db'
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'bamazon_db'
 });
 
 connection.connect();
 
 function placeOrder(id, quantity) {
-  var quanAvail;
-  var quan = parseInt(quantity);
-  var price;
-  var cost;
+    var quanAvail;
+    var quan = parseInt(quantity);
+    var price;
+    var cost;
 
 
-  connection.query('SELECT stock_quantity, price from products where id = ?', [id], function (error, results, fields) {
-    if (error) throw error;
-    quanAvail = parseInt(results[0].stock_quantity);
-    price = parseFloat(results[0].price);
-
-    if (quanAvail < quan) {
-      console.log("Sorry, we do not have enough stock for that order. Quantity available: " + quanAvail)
-    } else {
-
-      cost = quan * price;
-      connection.query('UPDATE products SET stock_quantity = stock_quantity - ? where id = ?', [quan, id], function (error, results, fields) {
+    connection.query('SELECT stock_quantity, price from products where id = ?', [id], function(error, results, fields) {
         if (error) throw error;
-        console.log("Your order has been placed. Your total came out to: $" + cost);
-      });
+        quanAvail = parseInt(results[0].stock_quantity);
+        price = parseFloat(results[0].price);
 
-      connection.query('INSERT INTO sales (product_id, quantity_purchased) VALUES (?, ?)', [id, quan], function (error, results, fields) {
-        if (error) throw error;
-      });
-    }
-  });
+        if (quanAvail < quan) {
+            console.log("Sorry, we do not have enough stock for that order. Quantity available: " + quanAvail)
+        } else {
+
+            cost = quan * price;
+            connection.query('UPDATE products SET stock_quantity = stock_quantity - ? where id = ?', [quan, id], function(error, results, fields) {
+                if (error) throw error;
+                console.log("Your order has been placed. Your total came out to: $" + cost);
+            });
+
+            connection.query('INSERT INTO sales (product_id, quantity_purchased) VALUES (?, ?)', [id, quan], function(error, results, fields) {
+                if (error) throw error;
+            });
+        }
+    });
 };
 
-connection.query('SELECT id, product_name, price from products', function (error, results, fields) {
-  if (error) throw error;
-  console.table(results);
-});
+//-------------------------program starts executing here----------------------------
 
-setTimeout(function () {
-  inquirer.prompt(
-    [{
-        type: "input",
-        name: "id",
-        message: "Please provide the id number of the product you would like to buy: "
-      },
-      {
-        type: "input",
-        name: "quantity",
-        message: "How many units would you like to purchase: "
-      }
-    ]
-  ).then(function (data) {
-    placeOrder(data.id, data.quantity);
-  });
-}, .5 * 1000);
+connection.query('SELECT id, product_name, price from products', function(error, results, fields) {
+    if (error) throw error;
+    console.table(results);
+    inquirer.prompt(
+        [{
+                type: "input",
+                name: "id",
+                message: "Please provide the id number of the product you would like to buy: "
+            },
+            {
+                type: "input",
+                name: "quantity",
+                message: "How many units would you like to purchase: "
+            }
+        ]
+    ).then(function(data) {
+        placeOrder(data.id, data.quantity);
+    });
+});
---
+++ bamazonManager.js
@@ -0,0 +1,125 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+require('console.table');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'bamazon_db'
+});
+
+connection.connect();
+
+function productsForSale() {
+
+    connection.query('SELECT id, product_name, price, stock_quantity from products', function(error, results, fields) {
+        if (error) throw error;
+        console.table(results);
+    });
+};
+
+function lowInventory() {
+
+    connection.query('SELECT id, product_name, price, stock_quantity from products where stock_quantity < 5', function(error, results, fields) {
+        if (error) throw error;
+        if (results.length > 0) console.table(results);
+        else console.log("There are no low inventory items to view.")
+    });
+};
+
+function addInventory() {
+    connection.query('SELECT id, product_name, stock_quantity from products', function(error, results, fields) {
+        if (error) throw error;
+        console.table(results);
+        setTimeout(function() {
+            inquirer.prompt(
+                [{
+                        type: "input",
+                        name: "id",
+                        message: "Add more inventory to id: "
+                    },
+                    {
+                        type: "input",
+                        name: "quantity",
+                        message: "Units: "
+                    }
+                ]
+            ).then(function(data) {
+                connection.query('UPDATE products SET stock_quantity = stock_quantity + ? where id = ?', [data.quantity, data.id], function(error, results, fields) {
+                    if (error) throw error;
+                    console.log("Inventory Updated");
+                    connection.query('SELECT id, product_name, stock_quantity from products where id = ?', [data.id], function(error, results, fields) {
+                        if (error) throw error;
+                        console.table(results);
+                    });
+
+                });
+            });
+        }, .5 * 1000);
+    });
+};
+
+function addProduct() {
+    inquirer.prompt(
+        [{
+                type: "input",
+                name: "prodName",
+                message: "Product Name: "
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "Department Id: "
+            },
+            {
+                type: "input",
+                name: "price",
+                message: "Price: "
+            },
+            {
+                type: "input",
+                name: "quan",
+                message: "Stock Qauntity: "
+            }
+
+
+        ]
+    ).then(function(data) {
+        connection.query('INSERT INTO products (product_name, department_id, price, stock_quantity) VALUES (?, ?, ?, ?)', [data.prodName, data.id, data.price, data.quan], function(error, results, fields) {
+            if (error) throw error;
+            console.log("Product added");
+            connection.query('SELECT * from products', [data.id], function(error, results, fields) {
+                if (error) throw error;
+                console.table(results);
+            });
+
+        });
+    });
+};
+
+//-------------------------program starts executing here----------------------------
+
+inquirer.prompt(
+    [{
+        type: "list",
+        name: "choice",
+        message: "Managers Menu",
+        choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
+    }]
+).then(function(data) {
+    switch (data.choice) {
+        case "View Products for Sale":
+            productsForSale();
+            break;
+        case "View Low Inventory":
+            lowInventory();
+            break;
+        case "Add to Inventory":
+            addInventory();
+            break;
+        case "Add New Product":
+            addProduct();
+            break;
+    }
+});
